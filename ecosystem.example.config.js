12367c5c431f4bc083cce5648a017a2ad4bf8953
Rey Mark A. Engada <mhikeking0@gmail.com>
2023-02-06 14:52:04 +0800

    Updated added pm2 and generated ecosystem config for pm2.

---
+++ ecosystem.example.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: 'socket',
+      script: 'build/bin/www.js',
+      env: {
+        NODE_ENV: 'development',
+        NODE_PORT: 3000,
+        REDIS_HOST: '127.0.0.1',
+        REDIS_PORT: 6279,
+        REDIS_USER: '',
+        REDIS_PASS: '',
+      },
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: true,
+      max_memory_restart: '150M',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      error_file: `${path.join(__dirname, 'pm2')}/app-error-${process.pid}.log`,
+      out_file: `${path.join(__dirname, 'pm2')}/app-out-${process.pid}.log`,
+      pid_file: `${path.join(__dirname, 'pm2')}/app-${process.pid}.log`,
+    },
+  ],
+};
