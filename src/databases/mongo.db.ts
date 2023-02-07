994fa923080dab4ed280f3c6860b5a0d13b3efb8
Rey Mark A. Engada <mhikeking0@gmail.com>
2023-02-07 13:51:52 +0800

    Updated mongodb connection config.

--- src/databases/mongo.db.ts
+++ src/databases/mongo.db.ts
@@ -1,10 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 const MONGO_HOST = (process.env.MONGO_DBNAME || 'localhost') as string;
-const MONGO_PORT = (process.env.MONGO_PORT || 27017) as number;
 export const MONGO_DBNAME = (process.env.MONGO_DBNAME || 's-adapter') as string;
-export const MONGO_USER = (process.env.MONGO_USER || 'root') as string;
-export const MONGO_PASS = (process.env.MONGO_PASS || 'admin123') as string;
 export const MONGO_COLLECTION = (process.env.MONGO_COLLECTION ||
   'socket.io-adapter-events') as string;
 
@@ -12,9 +9,7 @@ const mongo = async (): Promise<MongoClient | undefined> => {
   let mongoClient;
 
   try {
-    mongoClient = new MongoClient(
-      `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/`
-    );
+    mongoClient = new MongoClient(MONGO_HOST);
 
     await mongoClient.connect();
     console.log('MongoDB connected');
